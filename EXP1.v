bd3055e84b63b1dde3b9baed966fdc5872f784d7
shrivarshan-07 <shri31007@gmail.com>
2026-05-22 08:48:47 +0530

    exp completed

---
+++ EXP1.v
@@ -0,0 +1,21 @@
+module EXP1(
+    input A,
+    input B,
+    output AND_OUT,
+    output OR_OUT,
+    output NOT_OUT,
+    output NAND_OUT,
+    output NOR_OUT,
+    output XOR_OUT,
+    output XNOR_OUT
+);
+
+assign AND_OUT  = A & B;
+assign OR_OUT   = A | B;
+assign NOT_OUT  = ~A;
+assign NAND_OUT = ~(A & B);
+assign NOR_OUT  = ~(A | B);
+assign XOR_OUT  = A ^ B;
+assign XNOR_OUT = ~(A ^ B);
+
+endmodule
--- simulation/qsim/work/@g@a@t@e@s_@t@t_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity GATES_TT_vlg_vec_tst is
-end GATES_TT_vlg_vec_tst;
